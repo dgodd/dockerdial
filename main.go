d77cea5af0eab6d4b960ebbf16efabae63ec5225
Dave Goddard <dave@goddard.id.au>
2018-11-21 21:20:13 -0400

    remove debug print lines

--- main.go
+++ main.go
@@ -17,10 +17,8 @@ import (
 )
 
 func main() {
-	fmt.Println("DG: MAIN: 0")
 	dockerCli, err := dockercli.NewClientWithOpts(dockercli.FromEnv, dockercli.WithVersion("1.38"))
 	assertNil(err)
-	fmt.Println("DG: MAIN: 1")
 
 	// url := "http://example.com"
 	ctx := context.Background()
@@ -40,13 +38,11 @@ func main() {
 	}, nil, "")
 	assertNil(err)
 	defer dockerCli.ContainerKill(ctx, ctr.ID, "SIGKILL")
-	fmt.Println("DG: MAIN: 2")
 
 	r, err := CreateServerTar()
 	assertNil(err)
 	err = dockerCli.CopyToContainer(ctx, ctr.ID, "/", r, dockertypes.CopyToContainerOptions{})
 	assertNil(err)
-	fmt.Println("DG: MAIN: 3")
 
 	_, err = dockerCli.ContainerCommit(ctx, ctr.ID, dockertypes.ContainerCommitOptions{Reference: "dg"})
 	assertNil(err)
@@ -59,11 +55,9 @@ func main() {
 	})
 	assertNil(err)
 	defer res.Close()
-	fmt.Println("DG: MAIN: 4")
 
 	bodyChan, errChan := dockerCli.ContainerWait(ctx, ctr.ID, container.WaitConditionNextExit)
 	dockerCli.ContainerStart(ctx, ctr.ID, dockertypes.ContainerStartOptions{})
-	fmt.Println("DG: MAIN: 5")
 
 	pr, pw := io.Pipe()
 	go stdcopy.StdCopy(pw, os.Stderr, res.Reader)
@@ -76,27 +70,21 @@ func main() {
 	session, err := yamux.Client(&StdinStdout{in: pr, out: res.Conn}, nil)
 	assertNil(err)
 	go func() {
-		fmt.Println("DG: FUNC: 0")
 		c, err := session.Open()
 		if err != nil {
 			fmt.Println("ERR:", err)
 			return
 		}
-		fmt.Println("DG: FUNC: 1")
 
 		_, err = c.Write([]byte("GET / HTTP/1.1\nConnection: close\nHost: example.com\n\n"))
 		if err != nil {
 			fmt.Println("ERR:", err)
 			return
 		}
-		fmt.Println("DG: FUNC: 2")
 
 		io.Copy(os.Stdout, c)
-		fmt.Println("DG: FUNC: 3")
 		c.Close()
-		fmt.Println("DG: FUNC: 4")
 	}()
-	fmt.Println("DG: MAIN: 6")
 
 	select {
 	case body := <-bodyChan:
@@ -106,7 +94,7 @@ func main() {
 	case err := <-errChan:
 		fmt.Println("ERR: proxyDockerHostPort:", err)
 	}
-	fmt.Println("DG: MAIN: 7")
+	fmt.Println("DONE")
 }
 
 func assertNil(err error) {
